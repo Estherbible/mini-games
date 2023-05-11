569c5821d4cd588cdb147f90f188f8aba85bea89
Estherbible <115696413+Estherbible@users.noreply.github.com>
2023-05-11 18:24:43 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function redirect (game){
+    if (game=="snake") {
+        window.location="indexSnake.html"
+    }
+    else if (game== "tic tac") {
+        window.location="indexX,O.html"
+    }
+    else if (game=="bird") {
+        windows.location="indexbird.html"
+    }
+}
